4058de98d8d44d2208581cb7ba0f1e56f0ee9cee
DakotaTalley <dakota.talley@dakotatalley.com>
2025-04-17 21:21:05 -0400

    Add delta time and limit to 60FPS

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -13,6 +16,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
